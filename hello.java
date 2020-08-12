bf0ef31d84e3089d394c651322da6655456a53fc
nusrat ador <nusratador141462@gmail.com>
2020-08-12 09:31:13 -0700

    Added a new method called display to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class hello{
 	public static void public static void main(String[] args) {
 		System.out.printIn("Subl is Awesome...");
+
+		greeting("ns Ador");
 	}
+}
+
+public static void greeting(String name){
+	System.out.printIn("Welcome" + name);
 }
